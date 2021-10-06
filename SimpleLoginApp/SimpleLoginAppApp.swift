0a5663e6b2a685f9138433bac9178f4006a704f6
Dioniz <dioniz.bardhaj1@gmail.com>
2021-10-07 00:49:04 +0200

    Initial Commit

---
+++ SimpleLoginApp/SimpleLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginAppApp.swift
+//  SimpleLoginApp
+//
+//  Created by Dioniz Bardhaj on 7.10.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
